fcd23b294fcb496aefc87a805f50b973f1edbbf7
Matheus Veneziani <matheus.veneziani@taxceladdins.com.br>
2018-07-03 17:20:52 -0300

    terminando chamadas grpc

--- grpcclient/src/main/java/client/Main.java
+++ grpcclient/src/main/java/client/Main.java
@@ -1,8 +1,75 @@
 package client;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import grpc.Client;
+import protobufgencode.Aluno;
+import protobufgencode.Materia;
+import protobufgencode.Professor;
+import protobufgencode.Resultado;
+import protobufgencode.Turma;
 
 public class Main {
+	private static Turma t;
+	private static long[] d=new long[]{0,4, 9,10, 45, 6, 7, 8};
+	static {
+		List<Aluno> lista, apr,repr,rec;
+		lista = new ArrayList<Aluno>();
+		apr = new ArrayList<Aluno>();
+		repr = new ArrayList<Aluno>();
+		rec = new ArrayList<Aluno>();
+		
+		double media,mediaturma;
+		
+		mediaturma=0;
+		
+		List<Double> n;
+		Aluno alunoAux;
+		
+		Random ra = new Random();
+		for(int i=0; i<60; i++) {
+			n = new ArrayList<Double>();
+			media=0;
+			
+			for(int a=0; a<3; a++) {
+				n.add(ra.nextDouble()*10.0);
+			}
+			
+			for(Double d : n) {
+				media+=d;
+			}
+			
+			media = media/n.size();
+			
+			alunoAux=Aluno.newBuilder().setId(i).setMedia(media).setNome("Aluno"+i).addAllNotas(n).build();
+			
+			if(media<3.0) {
+				repr.add(alunoAux);
+			} else if(media<5.0) {
+				rec.add(alunoAux);
+			} else {
+				apr.add(alunoAux);
+			}
+			
+			mediaturma+=media;
+			
+			lista.add(alunoAux);
+		}
+		
+		mediaturma = mediaturma/lista.size();
+		
+		Professor p = Professor.newBuilder().setNome("Professor").setNome("NomeProfessorServidor").build();
+		
+		Materia m = Materia.newBuilder().setCodigo("SERVIDOR").setNome("Materia turma servidor").setDescricao("Descricao").build();
+		
+		t = Turma.newBuilder()
+				.setMateria(m)
+				.setProfessor(p)
+				.addAllAlunos(lista)
+				.build();
+	}
 	/**
 	   * Greet server. If provided, the first element of {@code args} is the name to use in the
 	   * greeting.
@@ -10,9 +77,40 @@ public class Main {
 	  public static void main(String[] args) throws Exception {
 	    Client client = new Client("localhost", 50051);
 	    try {
-	      client.runTests();
+	    	//client.testVoid();
+	    	
+	    	System.out.println("long;"+client.testLong(80)/1000000f);
+	    	
+	    	System.out.println("void;"+client.testVoid()/1000000f);
+			  
+			System.out.println("8long;"+client.testEightLong(80, -200, 400, 600, 9123, 2, -1233, 42)/1000000f);
+			  
+			List<Long> l = new ArrayList<Long>();
+			for(int i=0; i<d.length; i++) {
+				l.add(d[i]);
+			}
+			System.out.println("8longarray;"+client.testEightLongArray(l)/1000000f);
+			  
+			String s="";
+			System.out.println("stringlen;"+client.testStringLenght(s)/1000000f+";"+s.length());
+			s="a";
+			for(int i=0; i<22; i++) {
+				System.out.println("stringlen;"+client.testStringLenght(s)/1000000f+";"+s.length());
+				s=s+s;
+			}
+			 
+			System.out.println("complex;"+client.testComplexType(t)/1000000f);
+			  
+			System.out.println("collection;"+client.testCollection(t.getAlunosList())/1000000f);
+			  
+			System.out.println("arguments;"+client.testArgumentsRequest(t.getAlunos(0))/1000000f);
+			  
+			System.out.println("exception;"+client.testException()/1000000f);
+  
 	    } finally {
 	      client.shutdown();
 	    }
 	  }
+	  
+	  
 }
--- grpcclient/src/main/java/grpc/Client.java
+++ grpcclient/src/main/java/grpc/Client.java
@@ -33,10 +33,25 @@ package grpc;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import protobufgencode.Aluno;
+import protobufgencode.ArgumentsRequest;
+import protobufgencode.CollectionRequest;
+import protobufgencode.CollectionResponse;
+import protobufgencode.ComplexRequest;
+import protobufgencode.ComplexResponse;
 import protobufgencode.EPGrpc;
+import protobufgencode.EightLongArrayRequest;
+import protobufgencode.EightLongRequest;
 import protobufgencode.LongRequest;
 import protobufgencode.LongResponse;
+import protobufgencode.Resultado;
+import protobufgencode.StringRequest;
+import protobufgencode.StringResponse;
+import protobufgencode.Turma;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -55,7 +70,7 @@ public class Client {
   /** Construct client connecting to HelloWorld server at {@code host:port}. */
   public Client(String host, int port) {
     channel = ManagedChannelBuilder.forAddress(host, port)
-        .usePlaintext(true)
+        .usePlaintext()
         .build();
     blockingStub = EPGrpc.newBlockingStub(channel);
   }
@@ -66,58 +81,192 @@ public class Client {
 
   /** Say hello to server. */
   
-  public void testVoid(){
+  public long testVoid(){
+	  long start, finish;
 	  try {
-	      logger.info("Will try to run tests ");
+	      //logger.info("Will try to run tests ");
 	      Empty req = Empty.newBuilder().build();
 	      //marcar tempo
+	      start = System.nanoTime();
 	      blockingStub.testVoid(req);
+	      finish = System.nanoTime();
 	      //marcar tempo
-	      logger.info("Done");
+	      //logger.info("Done");
+	      return finish-start;
 	    } catch (RuntimeException e) {
-	      logger.log(Level.WARNING, "RPC failed", e);
-	      return;
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
 	    }
   }
   
   public long testLong(long val){
+	  long start, finish;
 	  try {
-	      logger.info("Will try to run tests ");
+	      //logger.info("Will try to run tests ");
 	      LongRequest req = LongRequest.newBuilder().setNumero(val).build();
 	      //marcar tempo
+	      start = System.nanoTime();
 	      LongResponse r = blockingStub.testLong(req);
+	      finish = System.nanoTime();
 	      //marcar tempo
-	      logger.info("Done");
-	      long resp = r.getResposta();
-	      return resp;
+	      //logger.info("Done");
+	      return finish-start;
 	    } catch (RuntimeException e) {
-	      logger.log(Level.WARNING, "RPC failed", e);
-	      return Long.MAX_VALUE;
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
 	    }
   }
   
-  public long testEightLong(long v1, long v2, long v3, long v4){
-	  return 0;
+  
+  public long testEightLong(long v1, long v2, long v3, long v4, long v5, long v6, long v7, long v8){
+	  long start, finish;
+	  try {
+	      //logger.info("Will try to run tests ");
+	      EightLongRequest req = EightLongRequest.newBuilder()
+	    		  .setParam1(v1)
+	    		  .setParam2(v2)
+	    		  .setParam3(v3)
+	    		  .setParam4(v4)
+	    		  .setParam5(v5)
+	    		  .setParam6(v6)
+	    		  .setParam7(v7)
+	    		  .setParam8(v8)
+	    		  .build();
+	      //marcar tempo
+	      start = System.nanoTime();
+	      LongResponse r = blockingStub.testEightLong(req);
+	      finish = System.nanoTime();
+	      //marcar tempo
+	      //logger.info("Done");
+	      return finish-start;
+	    } catch (RuntimeException e) {
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
+	    }
+	}
+  
+  public long testEightLongArray(List<Long> ar) {
+	  long start, finish;
+	  try {
+	      //logger.info("Will try to run tests ");
+	      EightLongArrayRequest req = EightLongArrayRequest.newBuilder()
+	    		  .addAllArray(ar)
+	    		  .build();
+	      //marcar tempo
+	      start = System.nanoTime();
+	      LongResponse r = blockingStub.testEightLongArray(req);
+	      finish = System.nanoTime();
+	      //marcar tempo
+	      //logger.info("Done");
+	      return finish-start;
+	    } catch (RuntimeException e) {
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
+	    }
   }
   
-  public void runTests() {
-    try {
-      logger.info("Will try to run tests ");
-      
-      Empty req = Empty.newBuilder().build();
-      //marcar tempo
-      blockingStub.testVoid(req);
-      //marcar tempo
-      
-      
-      
-      //HelloRequest request = HelloRequest.newBuilder().setName(name).build();
-      //HelloResponse response = blockingStub.sayHello(request);
-      logger.info("Done");
-    } catch (RuntimeException e) {
-      logger.log(Level.WARNING, "RPC failed", e);
-      return;
-    }
+  public long testStringLenght(String s) {
+	  long start, finish;
+	  try {
+	      //logger.info("Will try to run tests ");
+	      StringRequest req = StringRequest.newBuilder()
+	    		  .setReq(s)
+	    		  .build();
+	      //marcar tempo
+	      start = System.nanoTime();
+	      StringResponse r = blockingStub.testStringLength(req);
+	      finish = System.nanoTime();
+	      //marcar tempo
+	      //logger.info("Done");
+	      return finish-start;
+	    } catch (RuntimeException e) {
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
+	    }
+  }
+  
+  public long testComplexType(Turma t) {
+	  long start, finish;
+	  try {
+	      //logger.info("Will try to run tests ");
+	      ComplexRequest req = ComplexRequest.newBuilder()
+	    		  .setTurma(t)
+	    		  .build();
+	      //marcar tempo
+	      start = System.nanoTime();
+	      ComplexResponse r = blockingStub.testComplexType(req);
+	      finish = System.nanoTime();
+	      //marcar tempo
+	      //logger.info("Done");
+	      return finish-start;
+	    } catch (RuntimeException e) {
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
+	    }
+  }
+  
+  public long testCollection(Collection<Aluno> p){
+	  long start, finish;
+	  try {
+	      //logger.info("Will try to run tests ");
+	      CollectionRequest req = CollectionRequest.newBuilder()
+	    		  .addAllAlunos(p)
+	    		  .build();
+	      //marcar tempo
+	      start = System.nanoTime();
+	      CollectionResponse r = blockingStub.testRemoteCollection(req);
+	      finish = System.nanoTime();
+	      //marcar tempo
+	      //logger.info("Done");
+	      return finish-start;
+	  } catch (RuntimeException e) {
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
+	  }
+  }
+  
+  public long testArgumentsRequest(Aluno a) {
+	  long start, finish;
+	  try {
+	      //logger.info("Will try to run tests ");
+	      ArgumentsRequest req = ArgumentsRequest.newBuilder()
+	    		  .setNum(1).setNum2(Long.MAX_VALUE).setNum3(0.5f).setNum4(Double.MIN_VALUE).setBoolean(true).setStr("str")
+	    		  .setNum7(1).setNum8(Long.MAX_VALUE).setNum9(0.5f).setNum10(Double.MIN_VALUE).setBoolean2(true).setStr2("str")
+	    		  .setNum13(1).setNum14(Long.MAX_VALUE).setNum15(0.5f).setNum16(Double.MIN_VALUE).setBoolean3(true).setStr3("str")
+	    		  .setNum19(1).setNum20(Long.MAX_VALUE).setNum21(0.5f).setNum22(Double.MIN_VALUE).setBoolean4(true).setStr4("str")
+	    		  .setNum25(1).setNum26(Long.MAX_VALUE).setNum27(0.5f).setNum28(Double.MIN_VALUE).setBoolean5(true).setStr5("str")
+	    		  .setNum31(1).setNum32(Long.MAX_VALUE).setNum33(0.5f).setNum34(Double.MIN_VALUE).setBoolean6(true).setStr6("str")
+	    		  .setAluno(a)
+	    		  .build();
+	      //marcar tempo
+	      start = System.nanoTime();
+	      Empty r = blockingStub.testManyArguments(req);
+	      finish = System.nanoTime();
+	      //marcar tempo
+	      //logger.info("Done");
+	      return finish-start;
+	  } catch (RuntimeException e) {
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
+	  }
+  }
+  
+  public long testException() {
+	  long start, finish;
+	  try {
+	      //logger.info("Will try to run tests ");
+	      Empty req = Empty.newBuilder().build();
+	      //marcar tempo
+	      start = System.nanoTime();
+	      blockingStub.testException(req);
+	      finish = System.nanoTime();
+	      //marcar tempo
+	      //logger.info("Done");
+	      return finish-start;
+	    } catch (RuntimeException e) {
+	      //logger.log(Level.WARNING, "RPC failed", e);
+	      return -1;
+	    }
   }
 
   
